70ad8bc8c5999c58083cfb60312a46417e71d19b
JaapN <jaap.nieuwenhuizen@student.uva.nl>
2016-04-19 15:41:50 +0200

    Update fruitvliegen2.py

--- fruitvliegen2.py
+++ fruitvliegen2.py
@@ -1,5 +1,6 @@
 subset_initial = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9];
 subset_solution = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+counter = 0;
 
 for step in range(len(subset_initial)):
     if (subset_initial[step] != subset_solution[step]):
@@ -15,5 +16,5 @@ for step in range(len(subset_initial)):
         # insert swap list into subset_initial
         for m in range(len(swap)):
             subset_initial[step + m] = swap[m];
-
-print "Steps:", step;
+        counter += 1;
+print "Steps:", counter;
