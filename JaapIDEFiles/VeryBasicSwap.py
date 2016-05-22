07d8d7dd9b7e2b73e924757cb1ec1729852bbab5
JaapN <jaap.nieuwenhuizen@student.uva.nl>
2016-05-23 00:03:34 +0200

    The most basic
    Basic Swap without pattern recognition and functions/classes.

---
+++ JaapIDEFiles/VeryBasicSwap.py
@@ -0,0 +1,26 @@
+subset_initial = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
+subset_solution = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+counter = 0
+
+print "Drosophila Melanogaster: ", subset_initial
+print "Drosophila Miranda: ", subset_solution, "\n"
+
+for step in range(len(subset_initial)):
+    if (subset_initial[step] != subset_solution[step]):
+        # find and select the index of the number to be swapped with n
+        searchedI = subset_initial.index(subset_solution[step])
+        swap = []
+
+        # reversely iterate over the selected index to n
+        for i in range(searchedI, (step - 1), -1):
+            # append swapped row into swap list
+            swap.append(subset_initial[i])
+
+        # insert swap list into subset_initial
+        for m in range(len(swap)):
+            subset_initial[step + m] = swap[m]
+        counter += 1
+        print "Step", counter, ": ", subset_initial
+
+# print amount of steps
+print "Steps:", counter
