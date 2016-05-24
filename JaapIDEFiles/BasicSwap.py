e1871b83eb9f45abef69c9e142a38b430d048457
JaapN <jaap.nieuwenhuizen@student.uva.nl>
2016-05-25 00:19:02 +0200

    Basic without pattern recognition

--- JaapIDEFiles/BasicSwap.py
+++ JaapIDEFiles/BasicSwap.py
@@ -21,11 +21,11 @@ class BasicSwap(object):
         """
         Define initial variables. Turn initial and solution subset into global arrays.
         """
-
+        
         # the initial and solution subset
         self.subset_initial = subset_initial
         self.subset_solution = subset_solution
-
+        
         # keeps track of the number of steps
         self.counter = 0
 
@@ -34,7 +34,7 @@ class BasicSwap(object):
         Print an introduction and initialise the initial subset/genome and solution subset.
         The initial subset will have to be swapped around to get to the solution subset.
         """
-
+        
         # print introduction
         print "Initial: ", self.subset_initial
         print "Solution: ", self.subset_solution, "\n"
@@ -45,7 +45,7 @@ class BasicSwap(object):
         Initialise and reset all necessary helper variables for basic swapping.
         These variables will be inserted into other functions.
         """
-
+        
         # find and store the index of the number to be swapped with step into searchedI
         self.searchedI = (self.subset_initial).index(self.subset_solution[step])
         self.iSearch = self.searchedI
@@ -53,56 +53,23 @@ class BasicSwap(object):
         self.patlen = 0
         self.switcher = False
 
-    def swapPattern(self):
-        """
-        Search for a pattern/group in the subset.
-        If the subset is followed by incremental numbers, swap the highest number with the lowest.
-        """
-        
-        '''
-        This is pattern recognition.
-        '''
-        for recognition in range(len(self.subset_initial)):
-            if (self.searchedI != len(self.subset_initial) - 1):
-                if (self.subset_initial[self.searchedI] + 1 == self.subset_initial[self.searchedI + 1]):
-                    self.searchedI += 1
-                    self.patlen += 1
-                    self.switcher = True
-
-        '''
-        This is pattern swapping.
-        '''
-        # reversely iterate over the pattern
-        for pattern in range((self.iSearch + self.patlen), (self.iSearch - 1), -1):
-            # append swapped pattern row into swap list
-            (self.swap).append(self.subset_initial[pattern])
-
-        # insert swap list into subset_initial
-        for insert in range(len(self.swap)):
-            (self.subset_initial)[self.iSearch + insert] = self.swap[insert]
-
-        # increase counter by one and print swap
-        if (self.switcher == True):
-            self.counter += 1
-            print "Step", self.counter, ":", self.subset_initial
-
     def regularSwapping(self, step):
         """
         This is regular swapping.
         """
-
+        
         # clear swap array
         self.swap = []
-
+        
         # reversely iterate over the selected index to n
         for iterator in range(self.searchedI, (step - 1), -1):
             # append swapped row into swap list
             (self.swap).append(self.subset_initial[iterator])
-
+        
         # insert swap list into subset_initial
         for insert in range(len(self.swap)):
             self.subset_initial[step + insert] = self.swap[insert]
-
+        
         # increase counter by one and print swap
         self.counter += 1
         print "Step", self.counter, ":", self.subset_initial
@@ -123,18 +90,12 @@ class BasicSwap(object):
                 Initialize/reset variables.
                 '''
                 self.initVariables(step)
-
-                '''
-                Search for a pattern/group at the searched index (if one exists),
-                and inverse swap this group.
-                '''
-                self.swapPattern()
-
+                
                 '''
                 This is regular swapping.
                 '''
                 self.regularSwapping(step)
-
+        
         # print amount of steps
         print "Steps:", self.counter
 
